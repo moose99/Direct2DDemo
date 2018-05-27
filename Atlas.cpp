8b3297b08990cbe6436cb99b317a513ad88a69ea
moose99 <6894921+moose99@users.noreply.github.com>
2018-05-27 16:34:56 -0400

    updated to latest atlas code which supports variable sized images

--- Atlas.cpp
+++ Atlas.cpp
@@ -1,20 +1,29 @@
+/******************************************************************************
+ * Atlas.cpp: Atlas class method implementation.
+ *
+ * Copyright (C) 2018 SL Corporation.  All Rights Reserved.
+ * May 23, 2018
+ *****************************************************************************/
+
 #include <assert.h>
 #include "Atlas.h"
 
-Atlas::Atlas() :
-	m_pImageData(nullptr),
-	m_pBitmap(nullptr),
-	m_atlasWidth(0),
-	m_atlasHeight(0),
-	m_imageHeight(0),
-	m_imageWidth(0),
-	m_numImagesPerRow(0),
-	m_numImagesPerCol(0)
+
+Atlas::Atlas () :
+	m_pImageData( nullptr ),
+	m_pBitmap( nullptr ),
+	m_atlasWidth( 0 ),
+	m_atlasHeight( 0 ),
+	m_maxImageHeight( 0 ),
+	m_maxImageWidth( 0 ),
+	m_numImagesPerRow( 0 ),
+	m_numImagesPerCol( 0 ),
+	m_maxIndex( 0 )
 {
 
 }
 
-Atlas::~Atlas()
+Atlas::~Atlas ()
 {
 	Destroy();
 }
@@ -22,7 +31,8 @@ Atlas::~Atlas()
 //
 // Free bitmap and image data (device dependent resources)
 //
-void Atlas::Destroy()
+void 
+Atlas::Destroy ()
 {
 	if (m_pBitmap != nullptr)
 	{
@@ -31,16 +41,21 @@ void Atlas::Destroy()
 	}
 	delete[] m_pImageData;
 	m_pImageData = nullptr;
+
+	m_atlasWidth = 0;
+	m_atlasHeight = 0;
+	m_maxIndex = 0;
 }
 
 //
 // Create bitmap and imaga data from size (device dependent resources)
 //
-HRESULT Atlas::Create(ID2D1RenderTarget *deviceContext, int atlasWidth, int atlasHeight, int imageWidth, int imageHeight)
+HRESULT 
+Atlas::Create ( ID2D1RenderTarget *deviceContext, int atlasWidth, int atlasHeight, int maxImageWidth, int maxImageHeight )
 {
 	/// create bitmap
-	HRESULT hr = deviceContext->CreateBitmap(D2D1::SizeU(atlasWidth, atlasHeight),
-		D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED)), &m_pBitmap);
+	HRESULT hr = deviceContext->CreateBitmap( D2D1::SizeU(atlasWidth, atlasHeight),
+		D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED)), &m_pBitmap );
 	if (FAILED(hr))
 	{
 		return hr;
@@ -55,44 +70,48 @@ HRESULT Atlas::Create(ID2D1RenderTarget *deviceContext, int atlasWidth, int atla
 	m_atlasHeight = atlasHeight;
 
 	// store the width/height of the images in the atlas
-	m_imageWidth = imageWidth;
-	m_imageHeight = imageHeight;
+	m_maxImageWidth = maxImageWidth;
+	m_maxImageHeight = maxImageHeight;
 
 	// calculate the max number of images allowed per row and col
-	m_numImagesPerRow = int((float)atlasWidth / imageWidth);
-	m_numImagesPerCol = int((float)atlasHeight / imageHeight);
+	m_numImagesPerRow = int((float)atlasWidth / maxImageWidth);
+	m_numImagesPerCol = int((float)atlasHeight / maxImageHeight);
 	m_maxIndex = m_numImagesPerCol * m_numImagesPerRow - 1;
+
+	m_imageRects.resize( m_maxIndex );
 	return hr;
 }
 
 //
 // adds a binary pattern to the atlas at the specified index
 //
-void Atlas::AddPattern(bool *patternData, int imageIndex)
+void 
+Atlas::AddPattern ( bool *patternData, int imageIndex, int imageWidth, int imageHeight )
 {
 	// convert binary pattern to image data
-	uint32_t *imageData = new uint32_t[m_imageHeight * m_imageWidth];
+	uint32_t *imageData = new uint32_t[imageHeight * imageWidth];
 	int cnt = 0;
-	for (int i = 0; i < m_imageHeight; i++)
+	for (int i = 0; i < imageHeight; i++)
 	{
-		for (int j = 0; j < m_imageWidth; j++)
+		for (int j = 0; j < imageWidth; j++)
 		{
 			imageData[cnt] = patternData[cnt] == true ? 0xffffffff : 0x00000000;
 			cnt++;
 		}
 	}
 
-	AddImage(imageData, imageIndex);
+	AddImage( imageData, imageIndex, imageWidth, imageHeight );
 	delete[] imageData;
 }
 
 //
 // adds an image to the atlas at the specified index
 //
-void Atlas::AddImage(uint32_t *imageData, int imageIndex)
+void 
+Atlas::AddImage ( uint32_t *imageData, int imageIndex, int imageWidth, int imageHeight )
 {
 	assert(imageIndex >= 0 && imageIndex <= m_maxIndex);
-	assert(m_pImageData);
+	assert( m_pImageData );
 	if (m_pImageData == nullptr)
 		return;
 
@@ -101,45 +120,35 @@ void Atlas::AddImage(uint32_t *imageData, int imageIndex)
 	int imageRow = int((float)imageIndex / m_numImagesPerRow);
 
 	// compute atlas starting index
-	int atlasIndex = m_imageWidth * m_imageHeight * m_numImagesPerRow * imageRow + imageCol * m_imageWidth;
+	int atlasIndex = m_atlasWidth * m_maxImageHeight * imageRow + imageCol * m_maxImageWidth;
 	assert(atlasIndex < m_atlasWidth * m_atlasHeight);
 
+	m_imageRects[imageIndex] = D2D1::RectF(
+		(float)imageCol * m_maxImageWidth,			// left
+		(float)imageRow * m_maxImageHeight,			// top
+		(float)imageWidth + imageCol * m_maxImageWidth,		// right
+		(float)imageHeight + imageRow * m_maxImageHeight	// bot		
+	);
+
 	// copy image data into atlas at the correct spot
-	for (int i = 0; i < m_imageHeight; i++)
+	for (int i = 0; i < imageHeight; i++)
 	{
-		for (int j = 0; j < m_imageWidth; j++)
+		for (int j = 0; j < imageWidth; j++)
 		{
 			assert(atlasIndex < m_atlasWidth * m_atlasHeight);
 			m_pImageData[atlasIndex++] = *(imageData++);
 		}
-		atlasIndex += (m_numImagesPerRow - 1) * m_imageWidth;	// go to start of image at next row
+		atlasIndex += m_atlasWidth - imageWidth;	// go to start of image at next row
 	}
-}
-
-//
-// returns the rect of the image as vec4, given it's index
-//
-D2D1_VECTOR_4F Atlas::GetImageRectAsVec4(int imageIndex)
-{
-	assert(m_pImageData);
-
-	// compute image row and column from index
-	int imageCol = imageIndex % m_numImagesPerRow;
-	int imageRow = int((float)imageIndex / m_numImagesPerRow);
-
-	return D2D1::Vector4F((float)imageCol * m_imageWidth,	// left
-		(float)imageRow * m_atlasHeight,			// top
-		(float)m_imageWidth,						// width
-		(float)m_imageHeight);						// height		
 }
 
 //
 // returns the rect of the image given it's index
 //
-D2D1_RECT_F Atlas::GetImageRect(int imageIndex)
+void 
+Atlas::GetImageRect ( int imageIndex, D2D1_RECT_F *rect )
 {
-	D2D1_VECTOR_4F vec4 = GetImageRectAsVec4(imageIndex);
-	D2D1_RECT_F rect = { vec4.x /*left*/, vec4.y/*top*/, 
-		vec4.x + vec4.z/*right*/, vec4.y + vec4.w/*bot*/ };
-	return rect;
+	assert( m_pImageData );
+	*rect = m_imageRects[imageIndex];
 }
+
--- Atlas.h
+++ Atlas.h
@@ -1,8 +1,15 @@
+/******************************************************************************
+ * Atlas.h: Manage a bitmap atlas (a single bitmap containing other bitmaps).
+ *
+ * Copyright (C) 2018 SL Corporation.  All Rights Reserved.
+ * May 23, 2018
+ *****************************************************************************/
+
 #pragma once
 
 //
 // A square atlas which contains a series of images.
-// All images should be the same size.
+// All images do not have to be the same size, but they will occupy the size of the largest image dimensions
 // Images are referred to by index; the index starts at 0 as the upper left image, and then increases to the right 
 // and then down. 
 // ie. 0   1   2   3   4
@@ -12,32 +19,38 @@
 
 #include <d2d1_2.h>
 #include <cstdint>	// or stdint.h
+#include <vector>
+
 
 class Atlas
 {
+private:
+	std::vector<D2D1_RECT_F> m_imageRects;	// the rect of each image by index
+	ID2D1Bitmap *m_pBitmap;
+	uint32_t *m_pImageData;
+	int m_atlasWidth, m_atlasHeight;	// the width and height of the atlas
+	int m_maxImageHeight, m_maxImageWidth;	// the max width and height of the images
+	int m_numImagesPerRow, m_numImagesPerCol;
+	int m_maxIndex;
+
 public:
 	Atlas();
 	~Atlas();
 
-	void Destroy ();	// discard device resources
-	HRESULT Create(ID2D1RenderTarget *deviceContext,
+	void Destroy();	// discard device resources
+	HRESULT Create( ID2D1RenderTarget *deviceContext,
 		int atlasWidth, int atlasHeight,		// the size of the atlas bitmap
-		int imageWidth, int imageHeight);
- 
-	void AddImage(uint32_t *imageData, int imageindex);	// adds an image to the atlas at the specified index
-	void AddPattern(bool *patternData, int imageindex);	// adss a binary pattern at the specified index
+		int maxImageWidth, int maxImageHeight );
 
-	D2D1_VECTOR_4F GetImageRectAsVec4(int index);	// returns the rect of the image given it's index
-	D2D1_RECT_F GetImageRect(int index);			// returns the rect of the image given it's index
+	void AddImage( uint32_t *imageData, int imageindex, int imageWidth, int imageHeight );	// adds an image to the atlas at the specified index
+	void AddPattern( bool *patternData, int imageindex, int imageWidth, int imageHeight );	// adds a binary pattern at the specified index
 
-	ID2D1Bitmap *GetBitmap()	{ return m_pBitmap; }
-	const uint32_t *GetImageData() { return m_pImageData;  }
+	void GetImageRect( int index, D2D1_RECT_F *rect );		// returns the rect of the image given it's index
 
-private:
-	ID2D1Bitmap		*m_pBitmap;
-	uint32_t		*m_pImageData;
-	int m_atlasWidth, m_atlasHeight;
-	int m_imageHeight, m_imageWidth;	// the width and height of the images
-	int m_numImagesPerRow, m_numImagesPerCol;
-	int m_maxIndex;
+	ID2D1Bitmap *GetBitmap() { return m_pBitmap; }
+	const uint32_t *GetImageData() { return m_pImageData; }
+
+	int GetAtlasWidth() const { return m_atlasWidth;  }
+	int GetAtlasHeight() const { return m_atlasHeight;  }
 };
+
